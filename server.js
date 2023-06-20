3477369f77a8cf764cfde25b916d2e83783ec006
Arbaaz Mansuri <72750884+arbaaz1999@users.noreply.github.com>
2023-06-20 15:30:28 +0000

    health api created

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.get('/', async (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(PORT)
+app.listen(PORT, (err)=> {
+    if(err) console.log(err)
+    else console.log('App is successfully running on port : ', PORT)
+})
